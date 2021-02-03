65bc6bc8f179a523aebf174322d087669621b47e
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-02-02 20:00:42 -0600

    Local file paths instead of internet urls

--- categories.py
+++ categories.py
@@ -1,123 +1,102 @@
 import json
 
-import requests
 import pandas as pd
 import streamlit as st
 from pdf2image import convert_from_bytes
 
-REPO = "https://raw.githubusercontent.com/intelligent-environments-lab/dashboard/main"
 
-
-def _st_image(image_url=None, title=None, caption=None):
-    url = image_url
+def _st_image(image_path=None, caption=None):
     # NOTE: the order of title and data['title'] results in behavior that may not
     # be expected by other api users
     title = json.loads(
-        requests.get(
-            image_url[: image_url.rfind('.') + 1].format(plot_type='line_plot') + 'json'
-        ).content
+        open(
+            image_path[: image_path.rfind('.') + 1].format(plot_type='line_plot') + 'json', 'r'
+        ).read()
     )['title']
-    # title = data['title'] or title or None
     if title is not None:
         st.subheader(title)
 
     # Tells streamlit to create subheader with the given title followed by an
     # image from the provided url
-    if '{plot_type}' in image_url:
+    if '{plot_type}' in image_path:
         labels = {'Heatmap': 'heat_map', 'Line': 'line_plot'}
-        plot_type = labels[
-            st.selectbox('Plot type:', ['Heatmap', 'Line'], key=image_url)
-        ]
-        image_url = image_url.format(plot_type=plot_type)
+        plot_type = labels[st.selectbox('Plot type:', ['Heatmap', 'Line'], key=image_path)]
+        image_path = image_path.format(plot_type=plot_type)
 
     @st.cache(persist=False, allow_output_mutation=True, show_spinner=False, ttl=180)
     def download(image_url):
         data = json.loads(
-            requests.get(image_url[: image_url.rfind('.') + 1] + 'json').content
+            open(image_url[: image_url.rfind('.') + 1] + 'json', 'r').read()
         )
-        image = requests.get(image_url).content
+        image = open(image_url, 'rb').read()
         if image_url.endswith('.pdf'):
             image = convert_from_bytes(image)[0]
         return image, data
 
-    image, data = download(image_url)
+    image, data = download(image_path)
 
-    caption = caption or data['caption'] or image_url[image_url.rfind('/') + 1 :]
+    caption = caption or data['caption'] or image_path[image_path.rfind('/') + 1 :]
     caption += f' \n\n**Data source:** [{data["source"]["name"]}]({data["source"]["url"]})'
     st.image(image, use_column_width=True, output_format='png')
     st.markdown(caption)
 
 
-# def _plot_type(key):
-#     labels ={'Heatmap':'heat_map', 'Line':'line_plot'}
-#     plot_type = labels[st.selectbox('', ['Heatmap', 'Line'], key=key)]
-#     return plot_type
-
-
 class Economy:
-    ROOT = f'{REPO}/figures/economy'
+    ROOT = 'figures/economy'
 
     @staticmethod
     def consumer_spending(plot_type):
         # plot_type = st.selectbox('', ['heat_map', 'line_plot'], key='dsfasf')
         _st_image(
-            image_url=f'{Economy.ROOT}/consumer_spending/card_spending_change_{{plot_type}}.pdf',
-            title='Consumer Spending',
+            image_path=f'{Economy.ROOT}/consumer_spending/card_spending_change_{{plot_type}}.pdf',
         )
 
     @staticmethod
     def employment(plot_type):
         _st_image(
-            image_url=f'{Economy.ROOT}/employment/active_employees_change_{{plot_type}}.pdf',
-            title='Employment',
+            image_path=f'{Economy.ROOT}/employment/active_employees_change_{{plot_type}}.pdf',
         )
 
     @staticmethod
     def job_postings(plot_type):
         _st_image(
-            image_url=f'{Economy.ROOT}/job_postings/new_job_postings_change_by_job_zone_{{plot_type}}.pdf',
-            title='Job Postings',
+            image_path=f'{Economy.ROOT}/job_postings/new_job_postings_change_by_job_zone_{{plot_type}}.pdf',
         )
         _st_image(
-            image_url=f'{Economy.ROOT}/job_postings/new_job_postings_change_by_sector_{{plot_type}}.pdf'
+            image_path=f'{Economy.ROOT}/job_postings/new_job_postings_change_by_sector_{{plot_type}}.pdf'
         )
 
     @staticmethod
     def small_business_openings(plot_type):
         _st_image(
-            image_url=f'{Economy.ROOT}/small_business_opening/open_small_businesses_change_{{plot_type}}.pdf',
-            title='Small Business Openings',
+            image_path=f'{Economy.ROOT}/small_business_opening/open_small_businesses_change_{{plot_type}}.pdf',
         )
 
     @staticmethod
     def small_business_revenue(plot_type):
         _st_image(
-            image_url=f'{Economy.ROOT}/small_business_revenue/small_business_revenue_change_{{plot_type}}.pdf',
-            title='Small Business Revenue',
+            image_path=f'{Economy.ROOT}/small_business_revenue/small_business_revenue_change_{{plot_type}}.pdf',
         )
 
 
 class PublicHealth:
-    ROOT = f'{REPO}/figures/public_health'
+    ROOT = 'figures/public_health'
 
     @staticmethod
     def covid_19_case(plot_type=None):
         # st.subheader('Covid-19 Cases')
+        _st_image(image_path=f'{PublicHealth.ROOT}/covid_19_case/case_count_by_city_line_plot.pdf')
         _st_image(
-            image_url=f'{PublicHealth.ROOT}/covid_19_case/case_count_by_city_line_plot.pdf'
-        )
-        # if plot_type == 'heat_map':
-        #     st.write('\nKingsley mentioned on Monday that we should use the line plot instead of this one.')
-        _st_image(
-            image_url=f'{PublicHealth.ROOT}/covid_19_case/case_count_by_zip_code_line_plot.pdf'
+            image_path=f'{PublicHealth.ROOT}/covid_19_case/case_count_by_zip_code_line_plot.pdf'
         )
 
     @staticmethod
     def covid_19_policy():
         file = json.loads(
-            requests.get(
-                f'{PublicHealth.ROOT}/covid_19_policy/covid19_policies_unknown_figure_type.json'
-            ).content
+            open(
+                f'{PublicHealth.ROOT}/covid_19_policy/covid19_policies_unknown_figure_type.json',
+                'r',
+            ).read()
         )
         data = file['data']
         df = pd.DataFrame(data['data'], index=data['index'])
@@ -129,29 +108,26 @@ class PublicHealth:
 
 
 class Transport:
-    ROOT = f'{REPO}/figures/transport_and_mobility'
+    ROOT = 'figures/transport_and_mobility'
 
     @staticmethod
     def place_stay(plot_type):
         _st_image(
-            image_url=f'{Transport.ROOT}/place_stay/time_spent_and_visit_change_{{plot_type}}.pdf',
-            title='Place Stay',
+            image_path=f'{Transport.ROOT}/place_stay/time_spent_and_visit_change_{{plot_type}}.pdf',
         )
 
     @staticmethod
     def road_traffic(plot_type):
         _st_image(
-            image_url=f'{Transport.ROOT}/road_traffic/road_intersection_traffic_volume_change_{{plot_type}}.pdf',
-            title='Road Traffic',
+            image_path=f'{Transport.ROOT}/road_traffic/road_intersection_traffic_volume_change_{{plot_type}}.pdf',
         )
         if plot_type == 'line_plot':
             _st_image(
-                image_url=f'{Transport.ROOT}/road_traffic/road_intersection_traffic_volume_distribution_line_plot.pdf',
+                image_path=f'{Transport.ROOT}/road_traffic/road_intersection_traffic_volume_distribution_line_plot.pdf',
             )
 
     @staticmethod
     def transit_mode(plot_type):
         _st_image(
-            image_url=f'{Transport.ROOT}/transit_mode/direction_request_change_{{plot_type}}.pdf',
-            title='Transit Mode',
+            image_path=f'{Transport.ROOT}/transit_mode/direction_request_change_{{plot_type}}.pdf',
         )
